790c080d68826351aeb566859f49b50cad0df7a1
thebaptiste <jean-baptiste.veslin@meteo.fr>
2021-02-12 18:16:37 +0100

    build: fix repo name

--- drone.sh
+++ drone.sh
@@ -30,7 +30,7 @@ for REPO in $(echo "${PLUGIN_REPOSITORIES}" |sed 's/,/ /g'); do
     echo "Creating repository-dispatch event on REPO_NAME=${REPO_NAME} and BRANCH=${BRANCH_NAME}..."
     echo '{"event_type": "metwork-trigger", "client_payload": {"branch": "'${BRANCH_NAME}'"}}' > fic.json
     cat fic.json
-    curl -H "Accept: application/vnd.github.everest-preview+son" -H "Authorization: token ${GITHUB_TOKEN}" --request POST --data-binary @fic.json https://api.github.com/repos/metwork-framework/${REPO_NAME}/dispatches
+    curl -H "Accept: application/vnd.github.everest-preview+son" -H "Authorization: token ${GITHUB_TOKEN}" --request POST --data-binary @fic.json https://api.github.com/repos/${REPO_NAME}/dispatches
 
     if test $? -ne 0; then
         echo "ERROR during curl action"
